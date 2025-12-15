699cd6a39bfe85e5b075a0a8ce9076b4e0418ea0
Prakash <tirumani00000@gmail.com>
2025-12-15 21:49:58 +0530

    countoccurenceofnames

---
+++ Java8andCollections/src/com/nit/CountOccurenceOfNames.java
@@ -0,0 +1,24 @@
+package com.nit;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class CountOccurenceOfNames {
+
+	public static void main(String[] args) {
+		List<String> names=Arrays.asList("ramu","rowdy","raju","ramu","rowdy");
+		HashMap<String, Integer> hashMap = new HashMap<>();
+		System.out.println("Original Array: "+names);
+		for(String name:names) {
+			hashMap.put(name, hashMap.getOrDefault(name, 0)+1);
+			
+		}
+		
+		System.out.println("hashMap: "+hashMap);
+			
+		
+
+	}
+
+}
